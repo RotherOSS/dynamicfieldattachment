f6c34f048ca52c6e7119ab60011d0def61e5bf68
Stefan Härter <sha@rother-oss.com>
2026-01-07 07:47:35 +0100

    Removed unused variable in js.

--- var/httpd/htdocs/js/Core.Agent.DynamicField.Attachment.js
+++ var/httpd/htdocs/js/Core.Agent.DynamicField.Attachment.js
@@ -2,7 +2,7 @@
 // OTOBO is a web-based ticketing system for service organisations.
 // --
 // Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-// Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
+// Copyright (C) 2019-2026 Rother OSS GmbH, https://otobo.io/
 // --
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -27,8 +27,6 @@ Core.Agent.DynamicField = Core.AgentDynamicField || {};
  *      This namespace contains functions for all ticket action popups.
  */
 Core.Agent.DynamicField.Attachment = (function (TargetNS){
-
-    var filevalues = [];
 
     /**
      * @name Init
