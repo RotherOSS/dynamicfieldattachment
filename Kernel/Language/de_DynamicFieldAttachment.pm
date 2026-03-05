9541d6e4df1d0eaa2742dab4db311d2926c68c8e
Sven <svo@otobo.org>
2026-03-05 11:17:06 +0100

    Renamed language files.

--- Kernel/Language/de_DynamicFieldAttachment.pm
+++ Kernel/Language/de_DynamicFieldAttachment.pm
@@ -14,7 +14,7 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 # --
 
-package Kernel::Language::de_OTOBODynamicFieldAttachment;
+package Kernel::Language::de_DynamicFieldAttachment;
 
 use strict;
 use warnings;
--- Kernel/Language/hu_DynamicFieldAttachment.pm
+++ Kernel/Language/hu_DynamicFieldAttachment.pm
@@ -14,7 +14,7 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 # --
 
-package Kernel::Language::hu_OTOBODynamicFieldAttachment;
+package Kernel::Language::hu_DynamicFieldAttachment;
 
 use strict;
 use warnings;
