03c6dd3ed1188e1c26b8c0e830e2f4e7f7171160
Stefan Härter <sha@rother-oss.com>
2026-06-02 08:08:08 +0200

    Issue #8: Take process suffix into account for upload field id.

--- Kernel/System/DynamicField/Driver/Attachment.pm
+++ Kernel/System/DynamicField/Driver/Attachment.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2025 Rother OSS GmbH, https://otobo.io/
+# Copyright (C) 2019-2026 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -244,7 +244,7 @@ sub ValueSet {
 
     # get uploadcache object
     my $UploadCacheObject = $Kernel::OM->Get('Kernel::System::Web::UploadCache');
-    my $UploadFieldUID    = $Self->{ 'UploadCacheFormID' . $FieldName };
+    my $UploadFieldUID    = $Self->{ 'UploadCacheFormID' . $FieldName . ( $Param{DynamicFieldConfig}{ProcessSuffix} // '' ) };
     my $FormID;
 
     my @Attachments;
